1f56c418ce7ff6bfd8eb20874296e0433be1612b
jephdanso <jephdanso5@gmail.com>
2018-12-14 16:36:57 +0000

    deleted duplicat file

--- input_validation.js
+++
@@ -1,8 +0,0 @@
-// Disabble all non numeric characters
-function isNumber(evt) {
-    var iKeyCode = (evt.which) ? evt.which : evt.keyCode
-    if (iKeyCode != 46 && iKeyCode > 31 && (iKeyCode < 48 || iKeyCode > 57))
-    return false;
-    
-    return true;
-}
